2a3b1c9cc97ce476a01b07db549811f546eddaa6
camusant <antoine.camus@grenoble-inp.org>
2020-04-14 15:23:33 +0200

    test Part2_1 #2

--- PattRecClasses_Antoine/testPart2_1.m
+++ PattRecClasses_Antoine/testPart2_1.m
@@ -2,5 +2,16 @@
 %% Definition of the variables
 
 %Markov chain
-mcTest = MarkovChain([0.75 0.25],[0.99 0.01 ;0.03 0.97]);
+T=10000;
+mcTest1 = MarkovChain([0.75 0.25],[0.99 0.01 ;0.03 0.97]);
+pDgen(1)=GaussD('Mean',0 ,'StDev', 1);
+pDgen(2)=GaussD('Mean' , 3 ,'StDev',2);
 
+
+%% Question 2
+S=rand(mcTest1,T);
+tabulate(S)
+
+%    Results OK
+
+%% 
