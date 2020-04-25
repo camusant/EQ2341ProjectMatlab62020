030e3ef72e0e6d92314465856907819ceedd7492
JiaojiaowWu <62252818+JiaojiaowWu@users.noreply.github.com>
2020-04-26 00:32:58 +0800

    featureextract file

---
+++ Assignment2_SongRecognition_Wu_Camus/k_means_extract.m
@@ -0,0 +1,29 @@
+function [Semitone]=k_means_extract(frIsequence)
+[u re]=kmeans(frIsequence',2);  %u Data column; re: Clustering center
+
+for i=1:length(u)
+    if u(i)==1    
+         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'co'); 
+
+    else 
+         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'yo'); 
+    end
+end
+%Cluster center
+
+%get the pitches
+frIsequence0=[];
+n=1;
+for i=1:length(u)    
+    if u(i)==1
+        frIsequence0(n)=frIsequence(i);
+        n=n+1;
+    end  
+end
+minbound=min(frIsequence0);
+for i=1:length(u)
+    if frIsequence(i)<minbound
+        Semintone(i)=0
+    end
+    Semitone(i)=12*log2(frIsequence(i)-minbound)+1;
+end
