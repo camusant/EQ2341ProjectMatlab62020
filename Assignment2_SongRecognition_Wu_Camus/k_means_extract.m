411b1a91c4da90d0627bd6f8d554c35a77f2d513
JiaojiaowWu <62252818+JiaojiaowWu@users.noreply.github.com>
2020-04-26 21:09:03 +0800

    current work

--- Assignment2_SongRecognition_Wu_Camus/k_means_extract.m
+++ Assignment2_SongRecognition_Wu_Camus/k_means_extract.m
@@ -1,31 +1,37 @@
-function [Semitone]=k_means_extract(frIsequence)
-[u re]=kmeans(frIsequence',2);  %u Data column; re: Clustering center
+function [Semitone]=k_means_extract(frIsequence,k)
 
-for i=1:length(u)
-    if u(i)==1    
-         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'co'); 
-
-    else 
-         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'yo'); 
-    end
-    hold on
+if nargin == 1
+    k=2;
 end
-hold off
+
+[u re]=kmeans(frIsequence',k);  %u: Data column; re: Clustering center
+
+%for i=1:length(u)
+%    if u(i)==1    
+%         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'co'); 
+
+%    else 
+%         plot3(frIsequence(1,i),frIsequence(2,i),frIsequence(3,i),'yo'); 
+%    end
+%    hold on
+%end
+%hold off
 %Cluster center
 
 %get the pitches
 frIsequence0=[];
 n=1;
 for i=1:length(u)    
-    if u(i)==1
-        frIsequence0(n)=frIsequence(i);
+    if u(i)==2
+        frIsequence0(n)=frIsequence(1,i);
         n=n+1;
     end  
 end
 minbound=min(frIsequence0);
 for i=1:length(u)
-    if frIsequence(i)<minbound
-        Semintone(i)=0
+    if frIsequence(1,i)<minbound
+        Semintone(i)=0;
+    else
+    Semitone(i)=round(12*log2(frIsequence(1,i)/minbound))+1;
     end
-    Semitone(i)=12*log2(frIsequence(i)-minbound)+1;
 end
