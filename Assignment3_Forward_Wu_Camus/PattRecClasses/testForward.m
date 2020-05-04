1a2cf6b8d41042392ea59e4a0c1f72c64a8eae7f
JiaojiaowWu <62252818+JiaojiaowWu@users.noreply.github.com>
2020-05-05 01:41:07 +0800

    testforward

--- Assignment3_Forward_Wu_Camus/PattRecClasses/testForward.m
+++ Assignment3_Forward_Wu_Camus/PattRecClasses/testForward.m
@@ -1,13 +1,7 @@
-% testForward.m
-% Date : 2020-05-04
-
-clear all; close all;clc;
-%% Initialization
-
-q = [1 0]; %Initial State Probas
-A = [[0.9 0.1 0];[0 0.9 0.1]]; % Transition state probas
-mc = MarkovChain(q,A);
-x_obs = [-0.2, 2.6, 1.3];  %observed features
-
-%% Calculate the probabilities of the sequence
-
+mc=MarkovChain([1;0],[0.9 0.1 0;0 0.9 0.1]);
+X=[-0.2 2.6 1.3];
+pD1=GaussD; %b_1(x)
+pD2=GaussD('Mean',3,'StDev',2);
+pD=[pD1;pD2];
+pX=prob(pD, X)
+[alfaHat, c]=forward(mc,pX)
