f9bc6d47ec6bd8b45984c9f2b9a3329b1aac7899
camusant <antoine.camus@grenoble-inp.org>
2020-05-09 13:46:45 +0200

    Infinite duration test implemented
    I have taken the data from the problem 5.1. The results are good for both the forward and the logprob.

--- Assignment3_Forward_Wu_Camus/PattRecClasses/testForward.m
+++ Assignment3_Forward_Wu_Camus/PattRecClasses/testForward.m
@@ -1,3 +1,6 @@
+%% Finite duration test
+clear all;
+
 mc=MarkovChain([1;0],[0.9 0.1 0;0 0.9 0.1]);
 X=[-0.2 2.6 1.3];
 pD1=GaussD; %b_1(x)
@@ -7,3 +10,14 @@ pX=prob(pD, X);
 [alfaHat, c]=forward(mc,pX)
 h=HMM(mc,pD);
 logP=logprob(h,X)
+
+%% Infinite duration (taken from problem 5.1 of the course book)
+clear all;
+
+mc2=MarkovChain([1;0;0],[0.3 0.7 0; 0 0.5 0.5; 0 0 1]);
+B =DiscreteD([1 0 0 0; 0 0.5 0.4 0.1; 0.1 0.1 0.2 0.6]);
+z= [1 2 4 4 1];
+pZ=prob(B, z);
+[alfaHat2, c2]=forward(mc2,pZ)
+hmm2=HMM(mc2,B);
+logP2=logprob(hmm2,z)
