290db5f75435eb8ecf6c8559213fd1f56bf56356
JiaojiaowWu <62252818+JiaojiaowWu@users.noreply.github.com>
2020-05-28 16:51:31 +0800

    finalproject file

--- Final_Project/PattRecClasses/demo.m
+++ Final_Project/PattRecClasses/demo.m
@@ -1,18 +1,19 @@
 %% Recording voice
-Fs=22050
+Fs=22050;
 r=audiorecorder(Fs, 16, 1);
 disp('Please recording')
 recordblocking(r,5);     % hum into microphone...
 stop(r);
 disp('Record finish')
-p = play(r)   % listen to complete recording
+p = play(r) ;  % listen to complete recording
 
 %% show features
 S=string({'hum','voic'});
-frIseqtemp= GetMusicFeatures(r.getaudiodata,Fs)
+s=[1 2];
+frIseqtemp= GetMusicFeatures(r.getaudiodata,Fs);
 [Semi, frmin]=k_means_extract(frIseqtemp);    
 logP=hmms.logprob(Semi);
-plot((1:2),logP);
+plot(s,logP,'r');
 index=find(logP==max(logP));
 disp('This song is ')
 disp(S(index))
